80ed17b650eb8899138029aedcc57863436a42cd
kelvinhassan <ngerajustin@gmail.com>
2022-10-03 12:02:57 -0700

    multi language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-// Basic print with Javascript
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
